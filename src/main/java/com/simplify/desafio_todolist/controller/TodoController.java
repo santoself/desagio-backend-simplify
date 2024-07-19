e30987823e8c9601fdb24cf138cbb9262eec44a0
santoself <jesusfreakedson@gmail.com>
2024-07-19 10:32:07 -0300

    Implementação da camada controller

--- src/main/java/com/simplify/desafio_todolist/controller/TodoController.java
+++ src/main/java/com/simplify/desafio_todolist/controller/TodoController.java
@@ -1,2 +1,38 @@
-package com.simplify.desafio_todolist.controller;public class TodoController {
+package com.simplify.desafio_todolist.controller;
+
+import com.simplify.desafio_todolist.entity.Todo;
+import com.simplify.desafio_todolist.service.TodoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+
+    private final TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @PostMapping
+    List<Todo> create (@RequestBody Todo todo) {
+        return todoService.create(todo);
+    }
+
+    @GetMapping
+    List<Todo> list () {
+        return todoService.list();
+    }
+
+    @PutMapping
+    List<Todo> update (@RequestBody Todo todo) {
+        return todoService.update(todo);
+    }
+
+    @DeleteMapping("{id}")
+    List<Todo> delete (@PathVariable("id") Long id) {
+        return todoService.delete(id);
+    }
 }
